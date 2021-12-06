92512e6dafdf403bcd494a16f15c9cd5f3f73ba4
natsumi <nyny.sugar@gmail.com>
2021-12-06 22:10:44 +0900

    日本一わかりやすいTypeScript入門4途中

---
+++ src/basic/primitive.ts
@@ -0,0 +1,7 @@
+export default function primitiveSample() {
+  let name: string = 'トラハック'
+  console.log("primitive sample 1:", typeof name, name)
+
+  let age: number = 28
+  console.log("primitive sample 2:", typeof age, age)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import World from './world';
+// import World from './world';
 
-const root = document.getElementById("root")
-console.log(root)
-const world = new World("Hello World!");
-world.sayHello(root)
+// const root = document.getElementById("root")
+// console.log(root)
+// const world = new World("Hello World!");
+// world.sayHello(root)
+
+//03.基本の型定義
+import { primitiveSample } from './basic'
+
+primitiveSample()
