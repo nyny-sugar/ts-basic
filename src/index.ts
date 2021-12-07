bebed3d861b11082eccfbfaa6bd91acfd423cf22
natsumi <nyny.sugar@gmail.com>
2021-12-07 21:56:22 +0900

    日本一わかりやすいTypeScript入門5途中

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,20 @@
 // world.sayHello(root)
 
 //03.基本の型定義
-import {anySample, notExistSample, primitiveSample, unknownSample} from './basic'
+// import {anySample, notExistSample, primitiveSample, unknownSample} from './basic'
 
-anySample()
-notExistSample()
-primitiveSample()
-unknownSample()
+// anySample()
+// notExistSample()
+// primitiveSample()
+// unknownSample()
+
+// 04.関数の型定義
+import {logMessage} from './function/basic'
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice} from "./function/parameters";
+
+logMessage("Hello TypeScript!")
+isUserSignedIn( "ABC", "Torahack")
+isUserSignedIn("DEF")
+isUserSignedIn2("ABC")
+const sum = sumProductsPrice(100, 200, 300, 400, 500)
+console.log('Function parameters sample 5:', sum)
