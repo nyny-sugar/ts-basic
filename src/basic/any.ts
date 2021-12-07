95ea712605325c1efb3fed04a2b956736fc4c8c2
natsumi <nyny.sugar@gmail.com>
2021-12-07 14:48:30 +0900

    日本わかりやすいTypeScript入門4終了

---
+++ src/basic/any.ts
@@ -0,0 +1,8 @@
+export default function anySample() {
+  let name: any = 'Toragack' // string型を代入したよ
+  console.log("any sample 1:", typeof name, name)
+
+  name = 28
+  console.log("any sample 2:", typeof name, name)
+
+}
